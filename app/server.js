621de5f9ace8ba030d6539e1c1745b0444102261
dsalazarCazanhas <dsalazarcazanhas@gmail.com>
2026-04-03 22:00:44 -0600

    Update greeting message in server.js to include multiple languages

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world! | Hola, mundo! | Bonjour, le monde! | Hallo, Welt! | Ciao, mondo!');
 });
 
 app.listen(PORT, () => {
